4bb3dccd67730687aa7f2cc1fe8b3d1f6e7cc037
winfredselwyn <winfredselwyn@gmail.com>
2016-07-13 09:05:05 +0000

    Added first model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  namespace :api do
+    namespace :v1 do
+      resources :events, only: [:index, :create, :destroy, :update]
+    end
+  end
+
+  # resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times { Event.create!(title: "World War II", agenda: "Battle of The Bulge.") }
