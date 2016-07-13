12286aa280d9f208566cd2d1e7562280b54feb9c
winfredselwyn <winfredselwyn@gmail.com>
2016-07-13 13:08:49 +0000

    Before Tiddle experiment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :organized_events as: "Event"
+  
+  has_many :organized_events, class_name: "Event", foreign_key: "organizer_id"
+  
 end
