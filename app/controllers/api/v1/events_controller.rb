0277a08676700d30906787506f3d3df04f31fbdc
winfredselwyn <winfredselwyn@gmail.com>
2016-07-24 04:33:08 +0000

    Edited controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -35,6 +35,6 @@ class Api::V1::EventsController < Api::V1::BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :start_date, :end_date, :location, :agenda, :address, :guests, :host_name)
+      params.require(:event).permit(:title, :start_date, :end_date, :agenda, :address, :guests, :host_name)
     end
 end
