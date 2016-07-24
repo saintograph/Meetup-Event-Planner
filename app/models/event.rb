f6c39f206c0f56e659068f4f92fe0ab2845f355f
winfredselwyn <winfredselwyn@gmail.com>
2016-07-24 05:51:55 +0000

    Edited models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     #belongs_to :organizers, class_name: "User"
-    belongs_to :user
+    # belongs_to :user
     validates :title, presence: true
     # validates :start_date, presence: true
     # validates :end_date, presence:true
