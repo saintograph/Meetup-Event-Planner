09dbf6218cc77f2503eca7c7795c024ded28db09
winfredselwyn <winfredselwyn@gmail.com>
2016-07-24 05:50:05 +0000

    Edited models

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::EventsController < Api::V1::BaseController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   # before_action :set_event, only: [:show, :edit, :update, :destroy]
   # before_filter :authenticate_user!
 
